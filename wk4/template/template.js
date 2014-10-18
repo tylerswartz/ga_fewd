dc65d59d03f68414b14dec82839823df3fd9a78d
tylerswartz <tgswartz@gmail.com>
2014-10-18 16:54:28 -0700

    finished class js excercises

---
+++ wk4/template/template.js
@@ -0,0 +1,74 @@
+
+//var friends = ['Moe','Larry','Curly'];
+
+//friends.push('Tyler');
+//friends.push('Cher');
+
+//console.log(friends);
+
+//console.log(friends.length);
+
+//friends[3] = 'Swartz';
+
+//console.log(friends);
+
+//friends[4] = 'Jacques';
+
+//console.log(friends);
+
+//friends.splice(1,1);
+
+//console.log(friends);
+
+
+// var person = {
+// 	firstname: "John",
+// 	lastname: "Doe",
+// 	age: 36,
+// 	address: "1234 Park St",
+// 	city: "San Francisco",
+// 	state: "CA",
+// }
+
+// console.log(person);
+// console.log(person.age);
+
+var fivePeople = [
+	first = {
+		firstname: "John",
+		lastname: "Doe",
+		age: 36,
+		address: "1234 Park St",
+		city: "San Francisco",
+		state: "CA",
+	},
+	second = {
+		firstname: "Moe",
+		lastname: "Doe",
+		age: 31,
+		address: "1234 Park St",
+		city: "San Francisco",
+		state: "CA",
+	},
+	third = {
+		firstname: "Jane",
+		lastname: "Doe",
+		age: 26,
+		address: "1234 Park St",
+		city: "San Francisco",
+		state: "CA",
+	}
+]
+
+
+console.log(fivePeople[1].age);
+console.log((fivePeople[2].firstname) + " is " + (fivePeople[2].age) + " years old.");
+console.log(fivePeople[2].age);
+
+
+
+
+
+
+
+
