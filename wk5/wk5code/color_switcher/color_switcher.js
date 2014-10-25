fed8ce2a281b44eb05e99a80bddb1737a4cde064
tylerswartz <tgswartz@gmail.com>
2014-10-25 15:54:47 -0700

    finished dog code along

--- wk5/wk5code/color_switcher/color_switcher.js
+++ wk5/wk5code/color_switcher/color_switcher.js
@@ -1,5 +1,31 @@
 $(document).ready(function() {
-	// Put your code in here!
-
 	
+	$('#grayButton').click(function(){
+    $("body").attr('class','gray');
+    $("a").text('Search for a gray dog.');
+    $("a").attr('href', 'https://duckduckgo.com/?q=gray+dog')
+	});
+
+	$('#whiteButton').click(function(){
+    $("body").attr('class','white');
+    $("a").text("Search for a white dog.");
+    $("a").attr('href', 'https://duckduckgo.com/?q=white+dog')
+	});
+
+	$('#blueButton').click(function(){
+    $("body").attr('class','blue');
+    $("a").css('color','white');
+    $("a").text("Search for a blue dog.");
+    $("a").attr('href', 'https://duckduckgo.com/?q=blue+dog')
+	});	
+	
+	$('#yellowButton').click(function(){
+    $("body").attr('class','yellow');
+    $("a").css('color','black');
+    $("a").text("Search for a yellow dog.");
+    $("a").attr('href', 'https://duckduckgo.com/?q=yellow+dog')
+	});
+
+
 });
+
