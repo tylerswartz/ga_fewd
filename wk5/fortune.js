5dddef47546f13726fbd1c443cf5b63f00dd4265
tylerswartz <tgswartz@gmail.com>
2014-10-25 13:11:25 -0700

    fortune teller

---
+++ wk5/fortune.js
@@ -0,0 +1,35 @@
+// Step 1 & 2 HW
+// function predictFuture() {
+// 	fortunes = [];
+
+// 	for(var i = 0; i < 5; i++) {
+// 		fortunes.push(prompt("give me a potential fortune you'd like..."));
+// 	}
+
+// 	your_fortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+
+// 	alert("Your fortune is: " + your_fortune);
+
+// }
+
+
+// 
+// Addvanced
+
+var fortunes = [];
+
+function addPrediction() {
+	fortunes.push(document.getElementById('newPrediction').value);
+	document.getElementById('newPrediction').value = '';
+	var x = fortunes.length
+	document.getElementById('numFortunes').innerHTML = ' out of ' + x + ' fortunes.';
+}
+
+
+
+function predictFuture() {
+	your_fortune = fortunes[Math.floor(Math.random() * fortunes.length)]
+	alert("Your fortune is: " + your_fortune);
+	document.getElementById('numFortunes').innerHTML = '';
+	fortunes = [];
+}
--- wk5/template.js
+++ wk5/template.js
@@ -1,7 +1,26 @@
 var fruits = [];
 
+//loop and add elemnts to an array
 for(var i = 0; i < 5; i++) {
 	fruits.push(prompt("give me a fruit"));
 }
 
-alert("your fruits are: " + fruits);
+//loop through the array and do something to each member. in this case alert it.
+for(var i = 0; i < fruits.length; i++) {
+	alert(fruits[i]);
+}
+
+
+//to ask for fruits until stop and then just print out fruits
+var fruits = [];
+
+var new_fruit = prompt("just give me a fruit or tell me to stop...");
+
+while(new_fruit !=== "stop") {
+	fruit.push(new_fruit);
+	new_fruit = prompt("just give me a fruit or tell me to stop...");
+}
+
+for(var i = 0; i < fruits.length; i++) {
+	alert(fruits[i]);
+}
