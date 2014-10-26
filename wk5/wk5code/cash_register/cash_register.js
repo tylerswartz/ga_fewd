37fc403ae9d47cf0bf34e2aecbce37184cecc198
tylerswartz <tgswartz@gmail.com>
2014-10-25 17:14:46 -0700

    start cash register hw

--- wk5/wk5code/cash_register/cash_register.js
+++ wk5/wk5code/cash_register/cash_register.js
@@ -1,5 +1,33 @@
-$(document).ready(function() {
-	// Put your code in here!
+var current_total
+var input
 
+$(document).ready(function() {
+	//define array
+	current_total = 0
 	
+
+	//take submit input
+	$("#entry").submit(function(){
+		
+
+		//get text/value out
+		input = parseFloat($("#newEntry").val());
+
+		//parseFloat, return number
+
+		//check number
+	
+		//add to current total
+		current_total = current_total + input;
+
+		//add new row to table
+	
+		//sum up array and display as total
+
+		//clear input
+		return false;
+	});	
+
+
+		
 });
