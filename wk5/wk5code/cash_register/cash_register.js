be51d507647ab1bc0b28a1e08dbed5dbf22b6584
Tyler Swartz <tgswartz@gmail.com>
2014-10-27 07:54:08 -0700

    working on cash register hw

--- wk5/wk5code/cash_register/cash_register.js
+++ wk5/wk5code/cash_register/cash_register.js
@@ -2,32 +2,52 @@ var current_total
 var input
 
 $(document).ready(function() {
-	//define array
-	current_total = 0
+	//start total
+	current_total = 0;
 	
+	//remove chrome autocomplete dropdown from appearing.
+	//code
 
 	//take submit input
-	$("#entry").submit(function(){
-		
+	$('#entry').keypress(function (e) {
+ 		var key = e.which;
+ 		if(key === 13) {
+  
+			//get text/value out
+			input = parseFloat($("#newEntry").val());
 
-		//get text/value out
-		input = parseFloat($("#newEntry").val());
+			// //check number
+			// while(isNaN(input)) {
 
-		//parseFloat, return number
+			// 	// give error msg
+			// 	$("#hidden").removeAttr(‘class’,'hidden');
+			// 	$("h2").attr(‘class’,'hidden');
 
-		//check number
-	
-		//add to current total
-		current_total = current_total + input;
+			// 	//clear input
+			// 	$'#newEntry').value = '';
 
-		//add new row to table
-	
-		//sum up array and display as total
+			// 	//ask for another number
+			// 	input = parseFloat($("#newEntry").val());
 
-		//clear input
-		return false;
+			// 	//remove error msg
+			// 	$("h2").attr(‘class’,'hidden');
+			// };
+
+			//add to current total
+			current_total = current_total + input;
+
+			//add new row to table
+			$("#entries tr:last").after('<tr><td></td><td>' + input + '</td></tr>');
+
+
+			//sum and display as total in two decimals
+			$("#total").html("$" + current_total.toFixed(2));
+
+			//clear input
+			// $('#newEntry').val = '';
+			$("#newEntry").focus("How much?");
+
+			return false;
+  	}
 	});	
-
-
-		
 });
