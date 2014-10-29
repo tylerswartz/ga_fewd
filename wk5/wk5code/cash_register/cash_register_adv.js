bf53d76807d6a6b39427ff8ae9dea38a7471f7fb
Tyler Swartz <tgswartz@gmail.com>
2014-10-29 07:46:31 -0700

    everything except total subtract

--- wk5/wk5code/cash_register/cash_register_adv.js
+++ wk5/wk5code/cash_register/cash_register_adv.js
@@ -1,6 +1,40 @@
+// var current_total
+// var price_input
+// var item_input
+
+// $(document).ready(function() {
+// 	current_total = 0;
+
+// 	$('#entry').keypress(function (e) {
+//  		var key = e.which;
+//  		if(key === 13) {
+  
+// 			price_input = parseFloat($("#priceEntry").val());
+// 			item_input = $("#itemEntry").val();
+
+// 			if($.isNumeric(price_input)) {
+// 				$(".error").attr('class','hidden');
+// 				current_total = current_total + price_input;
+// 				$("#entries tr:last").after('<tr><td>' + item_input + '</td><td>' + price_input.toFixed(2) + '</td></tr>');
+// 				$("#total").html("$" + current_total.toFixed(2));
+// 				$(':input','#entry').val('');
+// 			}
+
+// 			else {
+// 				$(".hidden").attr('class','error');
+// 				$(':input','#entry').val('');
+// 				price_input = parseFloat($("#priceEntry").val());
+// 			}
+
+// 			return false;
+//   	}
+// 	});	
+// });
+
 var current_total
 var price_input
 var item_input
+var subtract_item
 
 $(document).ready(function() {
 	current_total = 0;
@@ -15,7 +49,7 @@ $(document).ready(function() {
 			if($.isNumeric(price_input)) {
 				$(".error").attr('class','hidden');
 				current_total = current_total + price_input;
-				$("#entries tr:last").after('<tr><td>' + item_input + '</td><td>' + price_input.toFixed(2) + '</td></tr>');
+				$("#entries tr:last").after('<tr><td>' + item_input + '<img class="delete" src="delete_icon.png"></td><td>' + price_input.toFixed(2) + '</td></tr>');
 				$("#total").html("$" + current_total.toFixed(2));
 				$(':input','#entry').val('');
 			}
@@ -24,12 +58,20 @@ $(document).ready(function() {
 				$(".hidden").attr('class','error');
 				$(':input','#entry').val('');
 				price_input = parseFloat($("#priceEntry").val());
-			}
-
-			return false;
+			}	
   	}
-	});	
+
+		$('.delete').click(function(){
+	  	console.log("clicked");
+	  	subtract_item = parseFloat($(this).find('tr:last-child').val());
+	  	current_total = current_total - subtract_item;
+	  	$(this).closest("tr").remove();
+	  	$("#total").html("$" + current_total.toFixed(2));
+	  }); 
+
+
+	
+	});
+  	return false;
 });
-
-
 
