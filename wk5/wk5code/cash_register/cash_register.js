b03a496d5604e8eaba6e18f5d5ecf8b7b600fa82
Tyler Swartz <tgswartz@gmail.com>
2014-10-28 18:43:52 -0700

    fixed numeric test and first part of hw

--- wk5/wk5code/cash_register/cash_register.js
+++ wk5/wk5code/cash_register/cash_register.js
@@ -17,8 +17,24 @@ $(document).ready(function() {
 			input = parseFloat($("#newEntry").val());
 
 			// check number
-			while($.isNumeric(input)) {
+			if($.isNumeric(input)) {
+				//remove error msg if needed
+				$(".error").attr('class','hidden');
 
+				//add to current total
+				current_total = current_total + input;
+			
+				//add new row to table
+				$("#entries tr:last").after('<tr><td></td><td>' + input.toFixed(2) + '</td></tr>');
+
+				//sum and display as total in two decimals
+				$("#total").html("$" + current_total.toFixed(2));
+
+				//clear input
+				$(':input','#entry').val('');
+			}
+
+			else {
 				// give error msg
 				$(".hidden").attr('class','error');
 
@@ -28,23 +44,7 @@ $(document).ready(function() {
 				//ask for another number
 				input = parseFloat($("#newEntry").val());
 
-				//remove error msg
-				$(".error").attr('class','hidden');
-				
-			};
-
-			//add to current total
-			current_total = current_total + input;
-
-			//add new row to table
-			$("#entries tr:last").after('<tr><td></td><td>' + input + '</td></tr>');
-
-
-			//sum and display as total in two decimals
-			$("#total").html("$" + current_total.toFixed(2));
-
-			//clear input
-			$(':input','#entry').val('');
+			}
 
 			return false;
   	}
