be4a22f51e6da640d1ddb025f8c171c3b88ac965
tylerswartz <tgswartz@gmail.com>
2014-11-01 14:00:06 -0700

    finished basic part of game

--- wk6/amazing_race/amazing_race.js
+++ wk6/amazing_race/amazing_race.js
@@ -1,47 +1,41 @@
-// document ready
 $(document).ready(function() {
 	
-	//set variable that is counter (player_one_position)
 	var player_one_position = 0;
 	var player_two_position = 0;
-
-	// set start position
+	
 	$(".player-1 td").eq(0).addClass('active');
 	$(".player-2 td").eq(0).addClass('active');
 
+	function check_winner(){
+		if (player_one_position === 49 && player_two_position != 49) {
+			$(".winner").html("PLAYER ONE WINS!!");
+			$("#restart").removeClass('hidden');
+			$("#restart").addClass('show');
+		} else if (player_one_position != 49 && player_two_position === 49) {
+			$(".winner").html("PLAYER TWO WINS!!");
+			$("#restart").removeClass('hidden');
+			$("#restart").addClass('show');
+		}
+	};
 
-	// key down listen.
 	$("body").keydown(function(e) {
  		if(e.which === 65) {
 			$(".player-1 td").eq(player_one_position).removeClass('active');
 			player_one_position ++;
 			$(".player-1 td").eq(player_one_position).addClass('active');
+			check_winner();
+
 		} else if (e.which === 76) {
 			$(".player-2 td").eq(player_two_position).removeClass('active');
 			player_two_position ++;
 			$(".player-2 td").eq(player_two_position).addClass('active');
-		}
+			check_winner();
 
+		}
 	});
 
+	$("#restart").click(function(){
+		location.reload();
+	});
 
 });
-
-//set variable that is counter (player_one_position)
-
-
-// set start position
-// $(".player-1 td").eq(0).addClass('active');
-
-
-
-
-
-//increment counter
-
-// add class to counter td (8th td, then 9th td)
-
-// remove class from counter -1 td.
-// $(".player-1 td").first().removeClass('active');
-
-// win conditions
