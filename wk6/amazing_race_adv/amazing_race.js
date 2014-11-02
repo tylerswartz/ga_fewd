d2938274a5bae718c8297ae6a58512b09eca6a4e
tylerswartz <tgswartz@gmail.com>
2014-11-01 21:02:08 -0700

    finished bot automation

--- wk6/amazing_race_adv/amazing_race.js
+++ wk6/amazing_race_adv/amazing_race.js
@@ -4,10 +4,13 @@ $(document).ready(function() {
 	var player_two_position = 0;
 	var game_on = true;
 	var speed = 0;
+	var rand;
 	
+	//start positions
 	$(".player-1 td").eq(0).addClass('active');
 	$(".player-2 td").eq(0).addClass('active');
 
+	//check if the player or bot has reached the end.
 	function check_winner(){
 		if (player_one_position === 49 && player_two_position != 49) {
 			$(".winner").html("PLAYER ONE WINS!!");
@@ -25,6 +28,7 @@ $(document).ready(function() {
 		}
 	};
 
+	//Player one keypress on A
 	$("body").keydown(function(e) {
  		if(e.which === 65) {
 			$(".player-1 td").eq(player_one_position).removeClass('active');
@@ -35,20 +39,22 @@ $(document).ready(function() {
 
 	});
 
+	//BOT Program
 	setInterval(function(){
-		speed = Math.floor(Math.random() * (800 - 100 + 1)) + 100;
-		if (game_on === true){
-			$(".player-2 td").eq(player_two_position).removeClass('active');
-			player_two_position++;
-			$(".player-2 td").eq(parseFloat(player_two_position)).addClass('active');
-			check_winner();
+		if (game_on === true) {
+			rand = (Math.floor(Math.random() * (700 - 200 + 1)) + 200);
+			setTimeout(function(){
+				$(".player-2 td").eq(player_two_position).removeClass('active');
+				player_two_position++;
+				$(".player-2 td").eq(parseFloat(player_two_position)).addClass('active');
+				check_winner();
+			}, rand);
 		} else {
 			return false;
-		}
-	}, speed);
+		};
+	}, 100);		
 
-
-
+	//Restart the game and play again
 	$("#restart").click(function(){
 		location.reload();
 	});
