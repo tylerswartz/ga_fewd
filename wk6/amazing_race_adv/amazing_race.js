5fb5bbf76b5f05dca224794245b66ee73f5b6627
Tyler Swartz <tgswartz@gmail.com>
2014-11-04 07:19:09 -0800

    removed table from race game

--- wk6/amazing_race_adv/amazing_race.js
+++ wk6/amazing_race_adv/amazing_race.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 	
+	var player_one_rank = 0;
 	var player_one_position = 0;
-	var player_two_position = 0;
-	var game_on = true;
+	var player_two_rank = 0;
+	var player_two_position= 0;
+	var game_on = false;
 	var speed = 0;
 	var rand;
 	
@@ -10,16 +12,23 @@ $(document).ready(function() {
 	$(".player-1 td").eq(0).addClass('active');
 	$(".player-2 td").eq(0).addClass('active');
 
+	//start button for bot
+	$("#start").click(function(){
+		game_on = true;
+		$("#start").addClass('hidden');
+		$(".ready").addClass('hidden');
+	});
+
 	//check if the player or bot has reached the end.
 	function check_winner(){
-		if (player_one_position === 49 && player_two_position != 49) {
+		if (player_one_position === 98 && player_two_position != 98) {
 			$(".winner").html("PLAYER ONE WINS!!");
 			$("#restart").removeClass('hidden');
 			$("#restart").addClass('show');
 			game_on = false;
 			$("body").off("keydown");
 
-		} else if (player_one_position != 49 && player_two_position === 49) {
+		} else if (player_one_position != 98 && player_two_position === 98) {
 			$(".winner").html("PLAYER TWO WINS!!");
 			$("#restart").removeClass('hidden');
 			$("#restart").addClass('show');
@@ -31,28 +40,28 @@ $(document).ready(function() {
 	//Player one keypress on A
 	$("body").keydown(function(e) {
  		if(e.which === 65) {
-			$(".player-1 td").eq(player_one_position).removeClass('active');
-			player_one_position ++;
-			$(".player-1 td").eq(player_one_position).addClass('active');
+			player_one_rank ++;
+			player_one_position = player_one_position + 2;
+			$(".player-1").css('margin-left', player_one_position + '%');
 			check_winner();	
 		}
-
 	});
+	
 
 	//BOT Program
 	setInterval(function(){
-		if (game_on === true) {
-			rand = (Math.floor(Math.random() * (700 - 200 + 1)) + 200);
-			setTimeout(function(){
-				$(".player-2 td").eq(player_two_position).removeClass('active');
-				player_two_position++;
-				$(".player-2 td").eq(parseFloat(player_two_position)).addClass('active');
+		rand = (Math.floor(Math.random() * (700 - 200 + 1)) + 200);
+		setTimeout(function(){
+			if (game_on === true) {
+				player_two_rank ++;
+				player_two_position = player_two_position + 2;
+				$(".player-2").css('margin-left', player_two_position + '%');
 				check_winner();
-			}, rand);
-		} else {
-			return false;
-		};
-	}, 100);		
+			} else {
+				return false;
+			};
+		}, rand);
+	}, 160);		
 
 	//Restart the game and play again
 	$("#restart").click(function(){
